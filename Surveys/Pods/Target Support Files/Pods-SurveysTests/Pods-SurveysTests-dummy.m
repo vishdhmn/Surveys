b43daef69423d4d8b0bc97ad589a023b929d50cb
Vishal Dhiman <vish.dhmn@gmail.com>
2017-02-12 07:31:11 +0530

    CocoaPods Added

---
+++ Surveys/Pods/Target Support Files/Pods-SurveysTests/Pods-SurveysTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SurveysTests : NSObject
+@end
+@implementation PodsDummy_Pods_SurveysTests
+@end
---
+++ Surveys/Pods/Target Support Files/Pods-SurveysUITests/Pods-SurveysUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SurveysUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SurveysUITests
+@end
