936d477d69ab280c1f68beb7a1defaa94954caae
Vishal Dhiman <vish.dhmn@gmail.com>
2017-02-12 07:26:24 +0530

    Created new project.

---
+++ Surveys/Surveys/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Surveys
+//
+//  Created by Vishal Dhiman on 12/02/17.
+//  Copyright © 2017 Vishal Dhiman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Surveys/Surveys/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Surveys
+//
+//  Created by Vishal Dhiman on 12/02/17.
+//  Copyright © 2017 Vishal Dhiman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
