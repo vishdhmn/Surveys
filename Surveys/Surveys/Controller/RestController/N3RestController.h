be919cd142fc5f38825007c0df1be64d28e7edff
Vishal Dhiman <vish.dhmn@gmail.com>
2017-02-12 11:53:33 +0530

    Added AFNetworking pod

---
+++ Surveys/Surveys/Controller/RestController/N3RestController.h
@@ -0,0 +1,13 @@
+//
+//  N3RestController.h
+//  Surveys
+//
+//  Created by Vishal Dhiman on 12/02/17.
+//  Copyright © 2017 Vishal Dhiman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface N3RestController : NSObject
+
+@end
---
+++ Surveys/Surveys/Controller/RestController/N3RestController.m
@@ -0,0 +1,17 @@
+//
+//  N3RestController.m
+//  Surveys
+//
+//  Created by Vishal Dhiman on 12/02/17.
+//  Copyright © 2017 Vishal Dhiman. All rights reserved.
+//
+
+#import "N3RestController.h"
+#import "AFNetworking.h"
+
+@implementation N3RestController
+
+
+
+
+@end
